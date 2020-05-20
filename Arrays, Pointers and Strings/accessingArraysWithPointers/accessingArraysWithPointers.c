306fbfc8c1d56c411f72b6e9275e0f36a9e8177d
Abhishek Baliyan <abhishek.baliyan.code@gmail.com>
2020-05-20 09:40:02 +0530

    Example of Accessing Arrays with Pointers

---
+++ Arrays, Pointers and Strings/accessingArraysWithPointers/accessingArraysWithPointers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int myArray[] = {1, 96, -3, 314, 52, 74}, *ptr = NULL, i;
+    
+    // Pointing ptr pointer to first value of the array.
+    // It can also be written in this way:- "ptr = myArray" 
+    // Because myArray is an pointer.
+    ptr = &myArray[0];
+
+    printf("\n\n");
+
+    for (i = 0; i < 6; i++)
+    {
+        printf("myArray[%d] = %d\n", i, myArray[i]);
+        printf("Contents of Address ptr + %d = %d\n", i, *(ptr + i));
+    }
+    return 0;
+}
