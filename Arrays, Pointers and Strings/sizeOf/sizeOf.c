92bde7aae0cd38afb0e0846588dd9475dc5bd3b9
Abhishek Baliyan <abhishek.baliyan.code@gmail.com>
2020-06-01 11:00:27 +0530

    Added size of array in sizeOf.c

--- Arrays, Pointers and Strings/sizeOf/sizeOf.c
+++ Arrays, Pointers and Strings/sizeOf/sizeOf.c
@@ -7,7 +7,8 @@ int main(void)
     int_size = sizeof(int);
     char_size = sizeof(char);
     flt_size = sizeof(float);
-
+    int arr[7] = {5, 8, 10, 50, 55, 98, 354};
+    int size = sizeof(arr) / sizeof(int);   // sizeof(arr) = 28, sizeof(int) = 4, 28/4 = 7
     printf("int, char and float sizes are %d, %d, and %d bytes\n", int_size, char_size, flt_size);
 
     return 0;
