31d1716ba622756fc74b782cd03883d610bfe5e8
Abhishek Baliyan <abhishek.baliyan.code@gmail.com>
2020-06-26 10:14:57 +0530

    Insertion Sort Algorithm

---
+++ Fast Arithmetic, Searching and Sorting/Sorting 1 - Insertion, Merge/insertion-sort.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+void insertionSort(int[], int);
+
+int main(void)
+{
+    int arr[] = {7, 5, 9, 3, 2, 10, 1, 4};
+
+    insertionSort(arr, 8);
+
+    for (int i = 0; i < 8; ++i)
+    {
+        printf("%d\n", arr[i]);    
+    }
+
+    return 0;
+}
+
+void insertionSort(int arr[], int length)
+{
+    for (int i = 1; i < length; ++i)
+    {
+        for (int j = i; j >= 1; --j)
+        {
+            if (arr[j] < arr[j -1])
+            {
+                int temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
+            } else if (arr[j] >= arr[j - 1])
+            {
+                break;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,12 +2,9 @@
 
 ### Data-Structures & Algorithms examples using C
 
-
-
 Thanks to [NPTEL][nptel] for providing this [Course][course].
 
 ---
-
 
 [nptel]: https://nptel.ac.in/
 [course]: https://nptel.ac.in/courses/106/106/106106127/
